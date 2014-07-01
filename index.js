83deffe1c72294fe62a752c03a35129a82dc8b5b
Ionică Bizău <bizauionica@yahoo.com>
2014-07-01 21:56:16 +0300

    Listen for new posts and attach tags field

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ Debug._config.success = {
 
 // Some output
 Debug.log("Plugin inited", "success");
+
+// Add the new field to be validated
+Bloggify.form["new-post"].validate["tags"] = "string,non-empty"
+
+// Listen for new posts
+Bloggify.emitter.on("new-post", function (data) {
+
+    // Get form data and post
+    var formData = data.formData
+      , post = data.newPost
+      ;
+
+    // Attach tags field
+    post.tags = formData.tags.split(/[ ,]+/);
+});
