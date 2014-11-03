d05935591be124135e0a80e41672788b3b9ceba8
Ionică Bizău <bizauionica@yahoo.com>
2014-11-03 15:37:54 +0200

    Preparing 2.0.0

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// Define success
-Debug._config.success = {
-    color: "#00CF06"
-  , text: "SUCCESS"
-};
-
-// Some output
-Debug.log("Plugin inited", "success");
-
 // Add the new field to be validated
-Bloggify.form["new-post"].validate["tags"] = "string,non-empty"
+// TODO
+//Bloggify.form["new-post"].validate["tags"] = "string,non-empty"
 
 // Listen for new posts
-Bloggify.emitter.on("new-post", function (data) {
+// TODO
+//Bloggify.emitter.on("new-post", function (data) {
+//
+//    // Get form data and post
+//    var formData = data.formData
+//      , post = data.newPost
+//      ;
+//
+//    // Attach tags field
+//    post.tags = formData.tags.split(/[ ,]+/);
+//});
 
-    // Get form data and post
-    var formData = data.formData
-      , post = data.newPost
-      ;
-
-    // Attach tags field
-    post.tags = formData.tags.split(/[ ,]+/);
-});
-
-// Enable post tags
-Config.options = Config.options || {};
-Config.options.postTags = true;
+console.log("Hello from post tags");
